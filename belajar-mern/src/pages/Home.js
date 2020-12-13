0095fdf2111bf08536a1be2c52cc2995f0d6b568
Fajar-islami <ahmadfajarislami@protonmail.com>
2020-12-13 14:18:10 +0700

    upload

--- belajar-mern/src/pages/Home.js
+++ belajar-mern/src/pages/Home.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API_URL } from "../config/confg";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-const Home = () => {
-	return <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque officia labore suscipit distinctio nemo fuga iste quidem hic minima nostrum exercitationem quae error, laborum, debitis explicabo minus dignissimos unde soluta!</div>;
-};
+function Home() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		getUser();
+	}, []);
+
+	const getUser = async () => {
+		await axios
+			.get(`${API_URL}/user`)
+			.then((res) => {
+				setData(res.data.data);
+				console.log(res.data);
+			})
+			.catch((err) => console.log(err));
+	};
+
+	const deleteUser = (id) => {
+		axios
+			.delete(`${API_URL}/user/${id}`)
+			.then((res) => {
+				console.log(res.data);
+				getUser();
+			})
+			.catch((err) => console.log(err));
+	};
+
+	return (
+		<div className="row justify-content-center mt-5">
+			<div className="col-md-8">
+				<div className="card">
+					<div className="card-body">
+						<div className="table-responsive">
+							<table className="table table-bordered">
+								<thead>
+									<tr>
+										<th>Fullname</th>
+										<th>Email</th>
+										<th>Alamat</th>
+										<th>Action</th>
+									</tr>
+								</thead>
+								<tbody>
+									{data.map((item, key) => (
+										<tr key={key}>
+											<td>{item.fullname}</td>
+											<td>{item.email}</td>
+											<td>{item.alamat}</td>
+											<td>
+												<Link onClick={(e) => deleteUser(item._id)} className="btn btn-sm btn-danger">
+													Delete
+												</Link>
+											</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
 
 export default Home;
--- react REST-API/controllers/UserController.js
+++ react REST-API/controllers/UserController.js
@@ -2,54 +2,54 @@ const express = require("express");
 const User = require("../models/user");
 
 const index = async (req, res) => {
-  try {
-    const dataUser = await User.find();
-    res.status(200).send({ status: 200, message: "Sukses", data: dataUser });
-  } catch (error) {
-    res.status(500).send({ message: error });
-  }
+	try {
+		const dataUser = await User.find();
+		res.status(200).send({ status: 200, message: "Sukses", data: dataUser });
+	} catch (error) {
+		res.status(500).send({ message: error });
+	}
 };
 
 // Create
 const create = async (req, res) => {
-  try {
-    const dataUser = await User({
-      fullname: req.body.fullname,
-      email: req.body.email,
-      phone: req.body.phone,
-      alamat: req.body.alamat,
-    });
+	try {
+		const dataUser = await User({
+			fullname: req.body.fullname,
+			email: req.body.email,
+			alamat: req.body.alamat,
+			phone: req.body.phone,
+		});
 
-    dataUser.save();
-    res.status(200).send({ status: 200, message: "user berhasil ditambahkan", data: dataUser });
-  } catch (error) {
-    res.status(500).send({ message: error });
-  }
+		dataUser.save();
+		res.status(200).send({ status: 200, message: "user berhasil ditambahkan", data: dataUser });
+	} catch (error) {
+		res.status(500).send({ message: error });
+	}
 };
 
 // GET Detail
 const detail = async (req, res) => {
-  try {
-    const dataUser = await User.findONe({ _id: req.params.id });
+	try {
+		const dataUser = await User.findONe({ _id: req.params.id });
 
-    res.status(200).send({ status: 200, message: "sukses", data: dataUser });
-  } catch (error) {
-    res.status(500).send({ message: error });
-  }
+		res.status(200).send({ status: 200, message: "sukses", data: dataUser });
+	} catch (error) {
+		res.status(500).send({ message: error });
+	}
 };
 
 // Delete
 const destroy = async (req, res) => {
-  try {
-    const dataUser = await User.findByIdAndRemove({ _id: req.params.id });
-    if (!dataUser) {
-      res.status(200).send({ status: 200, message: "Data tidak ditemukan" });
-    }
+	try {
+		const dataUser = await User.findByIdAndRemove({ _id: req.params.id });
+		if (!dataUser) {
+			res.status(200).send({ status: 200, message: "Data tidak ditemukan" });
+		}
 
-    res.status(200).send({ status: 200, message: "Data data berhasil dihapus" });
-  } catch (error) {
-    res.status(500).send({ message: error });
-  }
+		res.status(200).send({ status: 200, message: "Data data berhasil dihapus" });
+	} catch (error) {
+		res.status(500).send({ message: error });
+	}
 };
 
 module.exports = { index, create, detail, destroy };
